195fc74a22371e1e6e5d80e330861fc9835bbb9f
Marco <46133335+Marco4413@users.noreply.github.com>
2024-01-26 21:40:15 +0100

    Fixed indentation in premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,34 +1,34 @@
 project "fmt"
-    kind "StaticLib"
-    language "C++"
-    cppdialect "C++20"
+   kind "StaticLib"
+   language "C++"
+   cppdialect "C++20"
 
-    -- defines "FMT_HEADER_ONLY=1"
-    includedirs "include"
+   -- defines "FMT_HEADER_ONLY=1"
+   includedirs "include"
 
-    files {
-        "include/fmt/args.h",
-        "include/fmt/chrono.h",
-        "include/fmt/color.h",
-        "include/fmt/compile.h",
-        "include/fmt/core.h",
-        "include/fmt/format.h",
-        "include/fmt/format-inl.h",
-        "include/fmt/os.h",
-        "include/fmt/ostream.h",
-        "include/fmt/printf.h",
-        "include/fmt/ranges.h",
-        "include/fmt/std.h",
-        "include/fmt/xchar.h",
+   files {
+      "include/fmt/args.h",
+      "include/fmt/chrono.h",
+      "include/fmt/color.h",
+      "include/fmt/compile.h",
+      "include/fmt/core.h",
+      "include/fmt/format.h",
+      "include/fmt/format-inl.h",
+      "include/fmt/os.h",
+      "include/fmt/ostream.h",
+      "include/fmt/printf.h",
+      "include/fmt/ranges.h",
+      "include/fmt/std.h",
+      "include/fmt/xchar.h",
 
-        "src/format.cc",
-        "src/os.cc",
-    }
-    
-	filter "configurations:Debug"
-        runtime "Debug"
-        symbols "on"
+      "src/format.cc",
+      "src/os.cc",
+   }
 
-    filter "configurations:Release"
-        runtime "Release"
-        optimize "on"
+   filter "configurations:Debug"
+      runtime "Debug"
+      symbols "on"
+
+   filter "configurations:Release"
+      runtime "Release"
+      optimize "on"
